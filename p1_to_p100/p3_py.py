0fa9de37650ae9587b206b1df28344e3ae40d06d
v-ek <vek@kth.se>
2016-03-05 23:17:11 +0100

    solved p3 with simple recursion

--- p1_to_p100/p3_py.py
+++ p1_to_p100/p3_py.py
@@ -1,23 +1,17 @@
 from __future__ import division
-import numpy as np
-# brute force again
-def is_prime(x):
-    if x >= 2:
-        for y in range(2,x):
-            if not ( x % y ):
-                return False
-    else:
-        return False
-    return True
 
-product = 600851475143
-divisor_start = int(np.floor(np.sqrt(product)))
-print max_divisor
-for divisor in range(max_divisor,product):
-    if product % divisor == 0:
-        quotient = int(product / divisor)
-        if is_prime(quotient):
-            print quotient
-            break
- 
+def find_largest_factor(num):
+    i=1
+    while True:
+        i += 1
+        if num % i == 0:
+            if num == i:
+                return num
+            remainder = num // i
+            next = find_largest_factor(remainder)
+            return next
+        
 
+num_to_factor = 600851475143
+largest_factor = find_largest_factor(num_to_factor)
+print largest_factor
