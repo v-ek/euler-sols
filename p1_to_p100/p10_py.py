a026d4b82c953719a97571a65ae2ae0ae3834f11
v-ek <vek@kth.se>
2016-03-08 22:47:44 +0100

    more problems solved

---
+++ p1_to_p100/p10_py.py
@@ -0,0 +1,15 @@
+# 
+import numpy as np
+def primesfrom2to(n):
+    """ Input n>=6, Returns a array of primes, 2 <= p < n """
+    sieve = np.ones(n/3 + (n%6==2), dtype=np.bool)
+    for i in xrange(1,int(n**0.5)/3+1):
+        if sieve[i]:
+            k=3*i+1|1
+            sieve[       k*k/3     ::2*k] = False
+            sieve[k*(k-2*(i&1)+4)/3::2*k] = False
+    return np.r_[2,3,((3*np.nonzero(sieve)[0][1:]+1)|1)]
+
+max_num = 2000000
+prime_array = primesfrom2to(max_num)
+print prime_array.sum()
